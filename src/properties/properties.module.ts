0f1e092107216177005c78ad3c24c394ff2de6eb
Edidiong Ndaobong <100199901+EddieBenn@users.noreply.github.com>
2025-04-22 11:38:21 +0100

    Revert "Api flow"

--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -5,10 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Property } from './entities/property.entity';
 import { FileUploadService } from 'src/utils/cloudinary';
 import { PropertyTenant } from './entities/property-tenants.entity';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Property, PropertyTenant]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Property, PropertyTenant])],
   controllers: [PropertiesController],
   providers: [PropertiesService, FileUploadService],
 })

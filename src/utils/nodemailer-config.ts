ecadebdee1216910653d89ae40e7c868f8f72ee4
AJ-droi <ajiriosiobe@gmail.com>
2025-06-11 11:53:54 +0100

    gmail

--- src/utils/nodemailer-config.ts
+++ src/utils/nodemailer-config.ts
@@ -17,15 +17,10 @@ dotenv.config();
 // });
 
 
-  export const transporter = nodemailer.createTransport({
-    host: 'wghp11.wghservers.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASSWORD,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+ export const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});

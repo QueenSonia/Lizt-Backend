3b92579d462e4be6bb07185ca6761ef9eb006609
poesitor <poesitor1@gmail.com>
2026-05-15 10:19:42 +0100

    fix jwt error

--- src/common-areas/common-areas.module.ts
+++ src/common-areas/common-areas.module.ts
@@ -5,10 +5,18 @@ import { CommonAreasService } from './common-areas.service';
 import { CommonAreasController } from './common-areas.controller';
 import { MaintenanceRequest } from '../maintenance-requests/entities/maintenance-request.entity';
 import { TeamMember } from '../users/entities/team-member.entity';
+import { Account } from '../users/entities/account.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CommonArea, MaintenanceRequest, TeamMember]),
+    // Account is needed by JwtAuthGuard to hydrate req.user from the JWT
+    // payload into the full Account entity.
+    TypeOrmModule.forFeature([
+      CommonArea,
+      MaintenanceRequest,
+      TeamMember,
+      Account,
+    ]),
   ],
   controllers: [CommonAreasController],
   providers: [CommonAreasService],
